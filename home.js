4b522e9aeb8a0d2cf888255bdc01939846a66a84
MrCheese556 <85973878+MrCheese556@users.noreply.github.com>
2023-05-02 21:32:45 -0400

    Gave amal his ad
    added bottom text sending users to amals yt channel

--- home.js
+++ home.js
@@ -7,7 +7,7 @@ document.body.onpointermove = event => {
     }, {duration: 3000, fill: "forwards"})   
 }
 
-const letters = "abcdefghijklmnopqrstuvwxyz";
+const letters = "abcdefghijklmnopqrstuvwxyz‡₤€";//©°‽⁇꛷ຯ꘏꘍।⳾ΦΣ
 var inmotion = false;
 document.querySelectorAll("h2").forEach(element => {
     element.onmouseover = event => {
@@ -24,7 +24,7 @@ document.querySelectorAll("h2").forEach(element => {
                 return event.target.dataset.value[index];
             }
 
-        return letters[Math.floor(Math.random() * 26)];
+        return letters[Math.floor(Math.random() * letters.length)];
         }) 
         .join("");
         
@@ -51,7 +51,33 @@ document.getElementsByClassName("magic")[0].onmouseover = event => {
                 return event.target.dataset.value[index];
             }
 
-        return letters[Math.floor(Math.random() * 26)];
+        return letters[Math.floor(Math.random() * letters.length)];
+        }) 
+        .join("");
+        
+        if(iterations >= event.target.dataset.value.length){
+            clearInterval(interval);
+            inmotion = false;
+        } 
+        iterations+= 1/3;
+        }, 30);
+    }
+}
+document.getElementsByClassName("magic")[1].onmouseover = event => {
+    if(inmotion === false) {
+        let iterations = 0;
+        inmotion = true;
+
+        const interval = setInterval(() => {
+        event.target.innerHTML = event.target.innerHTML.split("")
+        .map((letter, index) => {
+
+            if(index < iterations)
+            {
+                return event.target.dataset.value[index];
+            }
+
+        return letters[Math.floor(Math.random() * letters.length)];
         }) 
         .join("");
         
